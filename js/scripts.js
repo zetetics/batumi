18e0b8a92369f8ba89eb5bea772068a8dbeb44da
Vlad Vasiluk <vlad.vasiluk@gmail.com>
2015-08-15 17:41:52 +0400

    ver 2.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
-/*!
- * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
+
+
 
 (function($) {
     "use strict"; // Start of use strict
@@ -36,5 +33,5 @@
 
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
-
+    
 })(jQuery); // End of use strict
